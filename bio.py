249c936de0a3c25ffbef3fb73dd290f6fa06e2f7
Rishabh <rishabhyadavm07@gmail.com>
2022-01-04 12:28:02 +0530

    added first set of files

---
+++ bio.py
@@ -0,0 +1,8 @@
+name = "akash"
+age = 19
+department  = "law"
+salary = 12000
+print("my name is ",name)
+print("i am ",age,"years old")
+print("i am in ",department,"department")
+print("i have a salary of ",salary)
