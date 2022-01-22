240289b6c91f7c0e3a480e77afb1f4cb3193f24c
Rishabh <rishabhyadavm07@gmail.com>
2022-01-22 19:12:43 +0530

    edited excel file

---
+++ Problems/Excel.py
@@ -0,0 +1,7 @@
+import decimal
+def todays_date():
+    to_day = int(input("Enter the day today: "))
+    to_month = int(input("Enter the month: "))
+    to_year = int(input("Enter the year you are in: "))
+    today_date = print(to_day, "-", to_month, "-", to_year)
+    print(today_date)
--- Problems/Tst.py
+++ Problems/Tst.py
@@ -1,11 +1,7 @@
-import xlrd
-
-file_location = (r"C:\Users\Rishabh\PycharmProjects\Py-for-Infosec\Problems\Book2.xlsx")
-#giving file location
-
-workbook_test = xlrd.open_workbook(file_location)#opening file
-
-sheet_test = workbook_test.sheet_by_index(0)
-
-sheet_test.cell_value(0,0)
-
+import decimal
+to_day = int(input("Enter the day today: "))
+to_month = int(input("Enter the month: "))
+to_year = int(input("Enter the year you are in: "))
+today_date = print(to_day, "-", to_month, "-", to_year)
+str_todaydate = str(today_date)
+print(type(today_date))
